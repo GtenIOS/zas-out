955d2e38cf0aa6f6a95ffbf051c83aac7cc483a5
GtenIOS <jdevlani@yahoo.in>
2023-02-09 15:21:38 +0530

    Added `ExeType` enum

--- out.zig
+++ out.zig
@@ -1,2 +1,13 @@
 pub const elf = @import("elf");
 pub const macho = @import("macho");
+
+const builtin = @import("builtin");
+
+pub const ExeType = enum {
+    macho,
+    elf,
+    const Self = @This();
+    pub inline fn default() Self {
+        return if (builtin.target.isDarwin()) .macho else .elf;
+    }
+};
